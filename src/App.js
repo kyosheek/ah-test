e3806773cbb8a9bcf1ff9257292a9993a2309c85
kyosheek <31596992+kyosheek@users.noreply.github.com>
2020-01-21 13:47:42 +0300

    Add data extraction and table creation

--- src/App.js
+++ src/App.js
@@ -1,21 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: null,
+      headers: null,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://raw.githubusercontent.com/blmzv/ah-frontend-intern/master/profiles.json', {
+      method: 'get',
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({
+          isLoaded: true,
+          data: data,
+          headers: Object.keys(data[0]),
+        });
+      });
+  }
+
+  renderTableHeader() {
+
+  }
+
+  renderEntry(entry, index) {
+    var row = Array(entry.length).fill(null);
+    var pos = 0;
+    for (var key in entry) {
+      row[pos] = <td>{entry[key]}</td>;
+      pos += 1;
+    }
+    return row;
+  }
+
+  renderTableData() {
+    const data = this.state.data;
+    var entries = Array(data.length).fill(null);
+    this.state.data.map((entry, index) => {
+      entries[index] = <tr key={index}>{this.renderEntry(entry, index)}</tr>;
+    });
+    return entries;
+  }
+
   render() {
+    const { isLoaded, data } = this.state;
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      isLoaded ?
+      <div id="dTable">
+        <table id="table">
+          <tbody>
+            <tr>{this.renderTableHeader()}</tr>
+            {this.renderTableData()}
+          </tbody>
+        </table>
       </div>
-    );
+      :
+      <p id="awaitText">Data is loading</p>
+    )
   }
 }
 
 export default App;
+
+function loadData() {
+
+}
